6a67cff6ff4bdb1b32f732fbbfbbc1af98265388
Scudfly <scudfly@qq.com>
2024-06-25 21:33:45 +0800

    add material

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { MatCardModule } from '@angular/material/card'
 
 describe('AppComponent', () => {
   beforeEach(async () => {
