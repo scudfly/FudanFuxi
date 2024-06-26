778f880b6609fdf837e30b01dad3d52fd2dd8e40
Scudfly <scudfly@qq.com>
2024-06-26 22:08:19 +0800

    add navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fudanfuxi';
+
+  public currentSubject: string = "";
+  public currentMode: string = "";
+
+  public currentData: any;
+
+  constructor(private http: HttpClient) {
+    this.http.get('assets/data/english4.json').subscribe(response => {
+      this.currentData = response;
+    });
+  }
+
+  selectSubject(subject: string) {
+    this.currentSubject = subject;
+  }
+
+  selectMode(mode: string) {
+    this.currentMode = mode;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material/card'
+import { MatCardModule } from '@angular/material/card';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { MatCardModule } from '@angular/material/card'
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatCardModule
+    MatCardModule,
+    HttpClientModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
